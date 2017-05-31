a6af7ec1490ee6e5998b4d3b8491d1108cbd3b78
Taras Malynovsky <malynovsky@ukr.net>
2017-05-31 15:20:15 +0300

    31.05.17: (2)

---
+++ working files/inc/const_menu2_tu.h
@@ -0,0 +1,29 @@
+#ifndef __CONST_TU_MENU2__
+#define __CONST_TU_MENU2__
+
+#define NAME_TU_IN_SIGNALS_RU           \
+  "Блок.           "
+
+#define NAME_TU_IN_SIGNALS_UA           \
+  "Блок.           "
+
+
+#define NAME_TU_IN_SIGNALS_EN           \
+  "Block           "
+
+#define NAME_TU_IN_SIGNALS_KZ           \
+  "Блок.           "
+    
+#define NAME_TU_OUT_SIGNALS_RU          \
+  "Выход           "
+
+#define NAME_TU_OUT_SIGNALS_UA          \
+  "Вихід           "
+
+#define NAME_TU_OUT_SIGNALS_EN          \
+  "Output          "
+
+#define NAME_TU_OUT_SIGNALS_KZ          \
+  "Выход           "
+    
+#endif 
